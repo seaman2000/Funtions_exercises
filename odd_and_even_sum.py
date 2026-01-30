e6b527e42ba3f2d0773cd829003f5862b7851339
Georgi Angelov <gtangelov76@gmail.com>
2026-01-30 17:12:54 +0200

    Update odd_and_even_sum.py

--- odd_and_even_sum.py
+++ odd_and_even_sum.py
@@ -1,6 +1,16 @@
-def odd_even_sum(num: int) -> int:
+def odd_even_sum(num: str):
+    even_sum = 0
+    odd_sum = 0
+
+    for digit in num:
+        if int(digit) % 2 == 0:
+            even_sum += int(digit)
+        elif int(digit) % 2 != 0:
+            odd_sum += int(digit)
+
+    return even_sum, odd_sum
 
 
-
-
-number = int(input())
+number = input()
+even, odd = odd_even_sum(number)
+print(f"Odd sum = {odd}, Even sum = {even}")
