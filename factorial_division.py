def27a30471440fe1aff602a0c58d06ce9b54905
Georgi Angelov <gtangelov76@gmail.com>
2026-01-30 21:41:16 +0200

    Update factorial_division.py

---
+++ factorial_division.py
@@ -0,0 +1,18 @@
+def factorial(number: int):
+    result = 1
+    for digit in range(1, number + 1):
+        result *= digit
+    return result
+
+
+def factorial_division(a: int, b: int):
+    return factorial(a) / factorial(b)
+
+
+def main_input():
+    first_num = int(input())
+    second_num = int(input())
+    result = factorial_division(first_num, second_num)
+    print(f"{result:.2f}")
+
+main_input()
