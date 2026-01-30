76118a692372fafc88eb5db86a8cd65d25da4e93
Georgi Angelov <gtangelov76@gmail.com>
2026-01-30 15:14:55 +0200

    Create smallest_of_three_numbers.py

---
+++ smallest_of_three_numbers.py
@@ -0,0 +1,9 @@
+def smallest_number(list_of_numbers: list):
+    return min(list_of_numbers)
+
+first_number_= int(input())
+second_number_= int(input())
+third_number_= int(input())
+
+result = smallest_number([first_number_, second_number_, third_number_])
+print(result)
